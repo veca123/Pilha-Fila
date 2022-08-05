51beaf7cdf1e98605b239264c27ed5f4e5f504a7
veca123 <50869120+veca123@users.noreply.github.com>
2022-08-04 21:40:28 -0300

    Add files via upload

---
+++ fila.py
@@ -0,0 +1,56 @@
+class VetorFila:
+    CAPACIDADE = 10         # Controla a capacidade de todos as novas filas
+
+    def __init__(self):
+        #Cria a fila vazia
+        self._dado = [None] * VetorFila.CAPACIDADE 
+        self._tamanho = 0
+        self._frente = 0
+
+    def __len__(self):
+        #Mostra o tamanho da fila
+        return self._tamanho
+
+    def estavazia(self):          #Verifica se a pilha esta vazia
+        return self._tamanho == 0
+
+    def primeiro(self):
+        if self.is_empty():
+            print('pilha vazia')
+            return None
+        else:
+            return self._data[self._frente]
+    def dequeue(self):              #Remove um elemento da fila  
+        if self.estavazia():
+            print('fila vazia')
+            return None  
+        else:
+            resposta = self._dado[self._frente]
+            self._dado[self._frente] = None         # Garante que não colete lixo
+            self._frente = (self._frente + 1) % len(self._dado)
+            self._tamanho -= 1
+            return resposta
+
+    def enqueue(self, elem):          #Coloca um elemento na fila
+        if self._tamanho == len(self._dado):
+            self._resize(2 * len(self.dado))     # dobra o tamanho do vetor
+        avail = (self._frente + self._tamanho) % len(self._dado)
+        self._dado[avail] = elem
+        self._tamanho += 1
+
+fila = VetorFila()
+fila.enqueue(1)
+fila.enqueue(2)
+fila.enqueue(3)
+fila.enqueue(4)
+fila.enqueue(5)
+fila.enqueue(6)
+fila.enqueue(7)
+fila.enqueue(8)
+fila.enqueue(9)
+fila.enqueue(10)
+
+print('tamanho da fila:',fila.__len__()) #Exibe o tamanho da fila
+while not fila.estavazia():
+  print(fila.dequeue())
+print("Fila vazia")
---
+++ pilha.py
@@ -0,0 +1,70 @@
+#Pilha
+class Empty(Exception):
+    def __init__(self, valor):
+      self.valor = valor
+
+    def __str__(self):
+      return repr(self.valor)
+
+class VetorPilha:
+
+  def __init__(self):
+    self._dado = []                       
+
+  def __len__(self):
+    return len(self._dado)
+
+  def is_empty(self):
+    return len(self._dado) == 0
+
+  def push(self, elemento):
+    self._dado.append(elemento)                 
+
+  def top(self):
+    if self.is_empty():
+      print('pilha vazia')
+      return None
+    else:
+      return self._dado[-1]                 # o ultimo elemento da pilha
+
+  def pop(self):
+    if self.is_empty():
+      print('pilha vazia')
+      return None
+    else:
+      return self._dado.pop()               # retira o ultimo elemento
+
+if __name__ == '__main__':
+
+    def ExibePilha(p):
+        pilha_auxiliar = VetorPilha() # cria pilha auxiliar
+        print('[', end="")
+        while(not p.is_empty()): # se a pilha nao ficar vazia
+            print(p.top(), end=" ") # retorna o elemento do topo da pilha
+            pilha_auxiliar.push(p.pop()) # coloca o elemento na pilha auxiliar
+        while(not pilha_auxiliar.is_empty()):
+            p.push(pilha_auxiliar.pop())
+        pilha_auxiliar = None
+        print(']\n')
+
+        
+    pilha = VetorPilha()                 
+    pilha.push(5)                        
+    pilha.push(3)
+    pilha.push(7)                        
+    pilha.push(9)                                          
+    pilha.push(4)                                        
+    pilha.push(6)                        
+    pilha.push(8)
+    print("Retorna se a pilha esta cheia ou vazia:")                       
+    print(pilha.is_empty())             #se a pilha estiver cheia retorna falso e se a pilha estiver vazia retorna verdadeiro 
+    print()
+    pilha.pop()
+    print("Retorna o elemento que está no topo da pilha")
+    print(pilha.top())                    #retorna o elemento do topo da pilha
+    print()
+    print("Exibição da pilha:")     
+    ExibePilha(pilha)                     #exibe a pilha
+    print()
+    print("Mostra a quantidade dos elementos da pilha:") 
+    print(len(pilha))                     #quantidade de elementos na pilha
